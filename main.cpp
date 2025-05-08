f624841a0607e714f718fe31327305cc8e98e324
takuma416 <k024g1026@g.neec.ac.jp>
2025-05-08 15:26:19 +0900

    オーバーロード

--- main.cpp
+++ main.cpp
@@ -1,21 +1,14 @@
 #include<stdio.h>
-
-int calc(int a, int b) {
-	return a + b;
-}
-
-float calc(float d, float e) {
-	return d + e + 10;
+template<typename Type>
+Type Min(Type a, Type b) {
+	return static_cast<Type>(a + b);
 }
 
 int main() {
-	int x = 114;
-	int y = 514;
-	printf("%d\n", calc(x, y));
-
-	float l = 3.64f;
-	float m = 3.64f;
-	printf("%f\n", calc(l, m));
+	printf("%d\n", Min<int>(114, 514));
+	printf("%f\n", Min<float>(1.14f, 5.14f));
+	printf("%lf\n", Min<double>(1.14, 5.14));
+	printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ\n", Min<char>);
 
 	return 0;
 }

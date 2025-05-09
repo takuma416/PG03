0f66467381836a7dbcff1ce47e35aeedee76ae2a
takuma416 <k024g1026@g.neec.ac.jp>
2025-05-09 17:05:25 +0900

    再帰

--- main.cpp
+++ main.cpp
@@ -1,21 +1,28 @@
 #include <stdio.h>
 
-int Recursivel(int kyuuryou, int zikan, int kotei) {
+int Recursive1(int kyuuryou, int zikan, int kotei) {
 	if (kyuuryou >= kotei) {
 		printf("%dŠÔŒã‚É’´‚¦‚é", zikan);
 		return zikan;
 	}
 	printf("%d %d\n", kyuuryou, kotei);
 	kyuuryou = kyuuryou * 2 - 50;
+	kotei += 1072;
 
-	return(Recursivel(kyuuryou, ++zikan, kotei=1072));
+	return(Recursive1(kyuuryou, ++zikan, kotei));
 }
 
 int main() {
 	int saiki = 100;
 	int ippan = 1072;
 	int zikan = 0;
-	int result1 = Recursivel(saiki, zikan, ippan);;
+	int result1 = Recursive1(saiki, zikan, ippan);;
 
 	return 0;
 }
+
+
+
+
+
+

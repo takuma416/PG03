81449d35cd574f18fe6d86ce0b934a223f09f63d
takuma416 <k024g1026@g.neec.ac.jp>
2025-05-16 14:12:30 +0900

    オーバーロード

--- main.cpp
+++ main.cpp
@@ -15,4 +15,5 @@ int main() {
 	printf("%d\n", Min<int>(1, 2));
 	printf("%f\n", Min<float>(1.0f, 2.0f));
 	printf("%lf\n", Min<double>(3.14159265, 2.7182818281));
+	printf("%c\n", Min<char>('a', 'b'));
 }

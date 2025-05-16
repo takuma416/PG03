f1041e26d19976d8ee5a9dbd9e5eb39523e1dd9d
takuma416 <k024g1026@g.neec.ac.jp>
2025-05-16 16:26:38 +0900

    クラステンプレート

--- main.cpp
+++ main.cpp
@@ -6,15 +6,15 @@ class TemplateClass {
 public:
     // コンストラクタ
     TemplateClass(Type number1, Type2 number2) :
-        /*変数A*/(number1), /*変数B*/(number2) {
+        Number1(number1), Number2(number2) {
     }
 
     Type Min()
     {
-        if (/*変数A*/ < /*変数B*/)
-            return static_cast<Type>(/*変数A*/);
+        if (Number1 < Number2)
+            return static_cast<Type>(Number1);
         else
-            return static_cast<Type>(/*変数B*/);
+            return static_cast<Type>(Number2);
     }
 
 private:
@@ -31,23 +31,12 @@ int main() {
     TemplateClass<double, int> doubleIntTemplate(234.0, 123);
     TemplateClass<double, float> doubleFloatTemplate(400.1, 500.2f);
 
-    std::cout << "int(100)　と float(50.0f) を比べて小さい数字を返す：
-        " << /*intFloatテンプレート*/.Min() << std::endl;
-
-        std::cout << "int(80)　と double(20.0) を比べて小さい数字を返す：
-        " << /*intDoubleテンプレート*/.Min() << std::endl;
-
-        std::cout << "float(2.0f)　と int(18) を比べて小さい数字を返す：
-        " << /*FloatIntテンプレート*/.Min() << std::endl;
-
-        std::cout << "float(11.0f)　と double(3.5) を比べて小さい数字を返す：
-        " << /*FloatDoubleテンプレート*/.Min() << std::endl;
-
-        std::cout << "double(234.0)　と int(123) を比べて小さい数字を返す：
-        " << /*DoubleIntテンプレート*/.Min() << std::endl;
-
-        std::cout << "double(400.1)　と float(500.2) を比べて小さい数字を返す：
-        " << /*DoubleFloatテンプレート*/.Min() << std::endl;
+    std::cout << intFloatTemplate.Min() << std::endl;
+    std::cout << intDoubleTemplate.Min() << std::endl;
+    std::cout << floatIntTemplate.Min() << std::endl;
+    std::cout << floatDoubleTemplate.Min() << std::endl;
+    std::cout << doubleIntTemplate.Min() << std::endl;
+    std::cout << doubleFloatTemplate.Min() << std::endl;
 
         return 0;
 }

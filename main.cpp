94b6a0bc47ceb8a45e4ba452ae27f064ca473c3d
takuma416 <k024g1026@g.neec.ac.jp>
2025-08-10 21:35:16 +0900

    std::thread

--- main.cpp
+++ main.cpp
@@ -1,36 +1,23 @@
-#include <cstdio>
+#include <iostream>
+#include <thread>
+
 using namespace std;
 
-class Animal {
-public:
-    virtual void sound() const = 0;
-};
+void PrintThread(uint32_t num) {
 
-
-class Dog : public Animal {
-public:
-    void sound() const override {
-        printf("イヌ鳴き声：ワン\n");
-    }
-};
-
-
-class Cat : public Animal {
-public:
-    void sound() const override {
-        printf("ネコ鳴き声:ニャー\n");
-    }
-};
+	cout << "thread" << num << endl;
+}
 
 int main() {
-    Animal* animal1 = new Dog();
-    Animal* animal2 = new Cat();
 
-    animal1->sound();
-    animal2->sound();
+	thread t1(PrintThread, 1);
+	t1.join();
 
-    delete animal1;
-    delete animal2;
+	thread t2(PrintThread, 2);
+	t2.join();
 
-    return 0;
+	thread t3(PrintThread, 3);
+	t3.join();
+
+	return 0;
 }
